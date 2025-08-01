20d5ba3e9a69bc56a70fa7aa379a81d2316e3280
Mark Parker <msparker@sky.com>
2025-08-01 11:37:25 +0100

    added: wake button

---
+++ custom_components/vaca/button.py
@@ -0,0 +1,47 @@
+"""Wyoming button entities."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from .const import DOMAIN
+from .entity import VASatelliteEntity
+
+if TYPE_CHECKING:
+    from homeassistant.components.wyoming import DomainDataItem
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up switch entities."""
+    item: DomainDataItem = hass.data[DOMAIN][config_entry.entry_id]
+
+    # Setup is only forwarded for satellites
+    assert item.device is not None
+
+    async_add_entities(
+        [
+            WyomingSatelliteWakeButton(item.device),
+        ]
+    )
+
+
+class WyomingSatelliteWakeButton(VASatelliteEntity, ButtonEntity):
+    """Entity to represent if satellite is muted."""
+
+    entity_description = ButtonEntityDescription(
+        key="wake",
+        translation_key="wake",
+    )
+
+    async def async_press(self) -> None:
+        """Press the button."""
+        self._device.send_custom_action("wake")
